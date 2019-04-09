86d48ff277075e53d937a97b85005b9c4fa8a34f
Lonneke Dikmans <lonneke.dikmans@eproseed.com>
2019-04-09 21:50:16 +0200

    fixed consumer using streams

--- src/node/app/consumer.js
+++ src/node/app/consumer.js
@@ -4,6 +4,8 @@ const fmt = require('bunyan-format');
 const kafkaLog = KafkaAvro.getLogger();
 const shipper = require('./shipper');
 
+const kafkaConsumerGroup = "shipper-consumer1";
+
 var kafkaAvro;
 
 var kafkaBrokerVar = process.env.KAFKA_BROKER || 'localhost:9092';
@@ -12,6 +14,8 @@ var kafkaRegistryVar = process.env.KAFKA_REGISTRY || 'http://130.61.35.61:8081';
 //topics as they are defined on Kafka
 const ORDER_PICKED_TOPIC = process.env.KAFKA_ORDER_PICKED || 'soaring-soaring-orderpicked';
 const SHIPMENT_REQUEST_ISSUED_TOPIC = process.env.KAFKA_SHIPMENT_REQUEST_ISSUED_TOPIC || 'soaring-shipmentrequestissue';
+
+let topics = [ORDER_PICKED_TOPIC, SHIPMENT_REQUEST_ISSUED_TOPIC];
 
 exports.initKafkaAvro = function () {
 
@@ -27,30 +31,50 @@ exports.initKafkaAvro = function () {
     kafkaAvro.init()
             .then(function () {
                 logger.info('Kafka Avro Ready to use');
-                let orderPickedConsumer = kafkaAvro.addConsumer(ORDER_PICKED_TOPIC);
-                let issueShipmentRequestConsumer = kafkaAvro.addConsumer(SHIPMENT_REQUEST_ISSUED_TOPIC);
-
-                kafkaLog.addStream({
-                    type: 'stream',
-                    stream: fmt({
-                        outputMode: 'short',
-                        levelInString: true
-                    }),
-                    level: 'debug'
-                });
-
-
-
-                orderPickedConsumer.on('message', message => {
-                    logger.debug('we received a message ' + message);
-                    shipper.pickUp(message);
-                });
-
-
-
-                issueShipmentRequestConsumer.on('message', message => {
-                    logger.debug('we received a message ' + message);
-                    shipper.offerDelivery(message);
-                });
             });
+            
+
+    kafkaLog.addStream({
+        type: 'stream',
+        stream: fmt({
+            outputMode: 'short',
+            levelInString: true
+        }),
+        level: 'debug'
+    });
+
+kafkaAvro.getConsumer({
+  'group.id': kafkaConsumerGroup,
+  'socket.keepalive.enable': true,
+  'enable.auto.commit': true
+}).then(function(consumer) {
+
+        //Read messages
+        var stream = consumer.getReadStream(topics, {
+          waitInterval: 0
+        });
+
+        //Exit if error
+        stream.on('error', function(err) {
+          console.log(err);
+          process.exit(1);
+        });
+
+        //exist if error
+        consumer.on('error', function(err) {
+          console.log(err);
+          process.exit(1);
+        });
+
+        //start streaming data
+        stream.on('data', function(message) {
+            console.log('Received message:', JSON.stringify(message.parsed));
+            if(message.topic === ORDER_PICKED_TOPIC){
+                shipper.pickUp(message.parsed);
+            } 
+            else if(message.topic === SHIPMENT_REQUEST_ISSUED_TOPIC){
+                shipper.offerDelivery(message.parsed);
+            }
+        });
+    });
 };
