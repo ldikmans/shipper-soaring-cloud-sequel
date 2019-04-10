798c19f4d5b79d3f315b0811b5c6aa000a2adb9c
Lonneke Dikmans <lonneke.dikmans@eproseed.com>
2019-04-10 12:43:02 +0200

    price fixes

--- src/node/app/shipper.js
+++ src/node/app/shipper.js
@@ -2,8 +2,8 @@ const request = require('request');
 const shippermarketUrl = process.env.SHIPPERMARKET_URL || 'http://129.213.126.223:8011/shippermarketplace';
 const logger = require('./logger');
 const shipper = 'QuickSilver';
-const minPrice = 1; 
-const maxPrice = 2.50; 
+const minPrice = 100; 
+const maxPrice = 300; 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 exports.pickUp = function(message){
@@ -52,7 +52,7 @@ exports.offerDelivery = function(message){
 
 function createOffer(orderId){
      
-    let price =Math.floor(Math.random() * (+maxPrice - +minPrice)) + +minPrice;
+    let price = Math.floor(Math.random() * (+maxPrice - +minPrice)) + +minPrice/100;
     console.log('price: '+ price);
     let requestBody = {
 	'orderId': orderId,
