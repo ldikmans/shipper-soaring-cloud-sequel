54847577c649c2a41a12b772b2120cfcc0b4400b
Lonneke Dikmans <lonneke.dikmans@eproseed.com>
2019-04-10 13:34:57 +0200

    shipper in customer receive event

--- src/node/app/customer.js
+++ src/node/app/customer.js
@@ -2,12 +2,13 @@ const request = require('request');
 const shippermarketUrl = process.env.SHIPPERMARKET_URL || 'http://129.213.126.223:8011/shippermarketplace';
 const logger = require('./logger');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+const shipper = 'QuickSilver';
 
 exports.receiveDelivery = function(message){
      let orderId = message.orderId;
      console.log('sending an receive to the shippermarket for orderId:' + orderId);
      let requestBody = {
-         'shipper': message.shipper,
+         'shipper': shipper,
          'event': 'RECEIVE'
      };
      return request({
